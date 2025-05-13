09f4bd52f08a29d9832a07c08abc79cf1e7d5ad1
unknown <Amalliafad>
2025-05-12 21:22:14 -0700

    deskripsi perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tugasbottomnav/dashborad_screen.dart';
+import 'package:tugasbottomnav/dashborad_page.dart';
 
 void main() {
   runApp(const MyApp());
